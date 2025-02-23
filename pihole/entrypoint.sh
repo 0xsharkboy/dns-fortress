2bdcc2c08993ce0a852df0aa5be0a791b8d01c57
0xsharkboy <achille@0xsharkboy.dev>
2025-02-23 18:57:02 +0100

    try to fix entrypoint again

--- pihole/entrypoint.sh
+++ pihole/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 # Update root hints
-UNBOUND_LIB_DIR=/var/lib/unbound
-if [ ! -f "$UNBOUND_LIB_DIR" ]; then
-    mkdir -p $UNBOUND_LIB_DIR
-fi
 wget https://www.internic.net/domain/named.root -qO- | tee ${UNBOUND_LIB_DIR}/root.hints
 
 # Start unbound
